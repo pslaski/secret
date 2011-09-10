020e2a64fd8fc4cf2d1a3a3a2f8d7d088d72bf7c
pslaski <pslaski@inf.ug.edu.pl>
2011-09-10 17:47:12 +0200

    pierdolki

---
+++ grails-app/controllers/pl/secret/MainEventController.groovy
@@ -0,0 +1,6 @@
+package pl.secret
+
+class MainEventController {
+
+    static scaffold = MainEvent
+}
---
+++ test/unit/pl/secret/MainEventControllerTests.groovy
@@ -0,0 +1,17 @@
+package pl.secret
+
+import grails.test.*
+
+class MainEventControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
